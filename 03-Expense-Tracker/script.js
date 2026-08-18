f47ecb2f3d0d332cebc225c0f34237f7576bad93
dheerajsharma0328 <dheerajsharma0328@gmail.com>
2026-08-18 06:06:51 +0530

    write js code project complete phase 1 ready

---
+++ 03-Expense-Tracker/script.js
@@ -0,0 +1,266 @@
+const addTransaction = document.querySelector('.add-transaction');
+const transactionList = document.querySelector('.transaction-list');
+const recentTransactionClearButton = document.querySelector('.clear-all-button');
+const currentBalance = document.querySelector('.current-balance');
+const totalIncome = document.querySelector('.total-income');
+const totalExpense = document.querySelector('.total-expense');
+const totolTransaction = document.querySelector('.totol-transaction');
+
+
+
+
+const storedData = localStorage.getItem('data');
+const balanceDataValue = localStorage.getItem('balanceData');
+const storedCategoryData =localStorage.getItem("categoryData");
+
+const data = storedData ? JSON.parse(storedData) : [];
+const balanceData = balanceDataValue ? JSON.parse(balanceDataValue) : {
+    income: 0,
+    expense: 0,
+    balance: 0,
+    transactions: 0
+};
+const categoryData = storedCategoryData
+    ? JSON.parse(storedCategoryData)
+    : {
+        Food: {
+            amount: 0,
+            transactions: 0
+        },
+
+        Travel: {
+            amount: 0,
+            transactions: 0
+        },
+
+        Shopping: {
+            amount: 0,
+            transactions: 0
+        },
+
+        Bills: {
+            amount: 0,
+            transactions: 0
+        },
+
+        Entertainment: {
+            amount: 0,
+            transactions: 0
+        },
+        Salary: {
+            amount: 0,
+            transactions: 0
+        },
+        FreeLancing: {
+            amount: 0,
+            transactions: 0
+        },
+
+        Other: {
+            amount: 0,
+            transactions: 0
+        }
+    };
+recentTransaction();
+renderBalance();
+renderCategory()
+
+
+addTransaction.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const formData = new FormData(addTransaction);
+
+    const transaction = {
+        id: crypto.randomUUID(),
+        title: formData.get('title'),
+        amount: Number(formData.get('amount')),
+        category: formData.get('category-input-value'),
+        type: formData.get('type-input'),
+        date: formData.get('date-input-value'),
+    };
+
+
+    data.push(transaction);
+    localStorage.setItem('data', JSON.stringify(data));
+    recentTransaction();
+    category(formData);
+    
+    balance(formData);
+    addTransaction.reset();
+
+})
+
+function recentTransaction() {
+    transactionList.innerHTML = "";
+    data.forEach((recentTransactionData, index) => {
+        const row = document.createElement('div');
+        row.classList.add('transaction-row');
+
+        row.innerHTML = `
+        <div>${index + 1}</div>
+        <div>${recentTransactionData.title}</div>
+        <div>${recentTransactionData.category}</div>
+        <div>${recentTransactionData.amount}</div>
+        <div>${recentTransactionData.type}</div>
+        <div>${recentTransactionData.date}</div>
+        <select>
+        <option value="" class="edit-button"  selected disabled>Modify</option>
+        <option>Delete</option>
+        <option>Edit</option></select>
+        
+        `;
+
+        transactionList.appendChild(row)
+    })
+}
+
+
+
+recentTransactionClearButton.addEventListener('click', function (event) {
+    data.length = 0;
+
+    balanceData.income = 0;
+    balanceData.expense = 0;
+    balanceData.balance = 0;
+    balanceData.transactions = 0;
+
+    Object.keys(categoryData).forEach(function (category) {
+
+        categoryData[category].amount = 0;
+        categoryData[category].transactions = 0;
+
+    });
+
+    localStorage.removeItem('data');
+    localStorage.removeItem('balanceData');
+    localStorage.removeItem('categoryData');
+
+    recentTransaction();
+    renderBalance();
+    renderCategory();
+
+});
+
+function renderBalance() {
+    totalIncome.innerText = balanceData.income;
+    totalExpense.innerText = balanceData.expense;
+    currentBalance.innerText = balanceData.balance;
+    totolTransaction.innerText = balanceData.transactions;
+}
+
+function balance(formData) {
+
+    const amount = Number(formData.get('amount'));
+    const type = formData.get('type-input');
+    
+    if (type === 'income') {
+        
+        balanceData.income += amount;
+        balanceData.balance += amount;
+        
+    }
+    
+    if (type === 'expense') {
+        balanceData.expense += amount;
+        balanceData.balance -= amount;
+        
+    }
+    
+    balanceData.transactions += 1;
+    saveBalance();
+    renderBalance();
+}
+
+
+function saveBalance() {
+    localStorage.setItem('balanceData', JSON.stringify(balanceData))
+}
+
+function category(formData)
+{
+    const amount = Number(formData.get('amount'));
+    const category = formData.get('category-input-value');
+
+    categoryData[category].amount += amount;
+    categoryData[category].transactions += 1;
+    saveCategoryData();
+    renderCategory()
+
+}
+
+
+function saveCategoryData() {
+
+    localStorage.setItem('categoryData',JSON.stringify(categoryData));
+
+}
+
+
+
+function renderCategory() {
+
+    // Food
+    document.querySelector('.food-spent-amount').innerText =
+        categoryData.Food.amount;
+
+    document.querySelector('.food-total-transaction-number').innerText =
+        categoryData.Food.transactions;
+
+
+    // Travel
+    document.querySelector('.travel-spent-amount').innerText =
+        categoryData.Travel.amount;
+
+    document.querySelector('.travel-total-transaction-number').innerText =
+        categoryData.Travel.transactions;
+
+
+    // Shopping
+    document.querySelector('.shopping-spent-amount').innerText =
+        categoryData.Shopping.amount;
+
+    document.querySelector('.shopping-total-transaction-number').innerText =
+        categoryData.Shopping.transactions;
+
+
+    // Bills
+    document.querySelector('.bills-spent-amount').innerText =
+        categoryData.Bills.amount;
+
+    document.querySelector('.bills-total-transaction-number').innerText =
+        categoryData.Bills.transactions;
+
+
+    // Entertainment
+    document.querySelector('.entertainment-spent-amount').innerText =
+        categoryData.Entertainment.amount;
+
+    document.querySelector('.entertainment-total-transaction-number').innerText =
+        categoryData.Entertainment.transactions;
+
+
+    // Salary
+    document.querySelector('.salary-spent-amount').innerText =
+        categoryData.Salary.amount;
+
+    document.querySelector('.salary-total-transaction-number').innerText =
+        categoryData.Salary.transactions;
+
+
+    // FreeLancing
+    document.querySelector('.freelancing-spent-amount').innerText =
+        categoryData.FreeLancing.amount;
+
+    document.querySelector('.freelancing-total-transaction-number').innerText =
+        categoryData.FreeLancing.transactions;
+
+
+    // Other
+    document.querySelector('.other-spent-amount').innerText =
+        categoryData.Other.amount;
+
+    document.querySelector('.other-total-transaction-number').innerText =
+        categoryData.Other.transactions;
+
+}
